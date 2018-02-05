e8dab9f4f454d54b60937f975025e7860f1acb66
nashaofu <diaocheng@outlook.com>
2018-02-05 11:19:57 +0800

    多语言修改，并编写文档

---
+++ examples/views/locale/index.vue
@@ -0,0 +1,19 @@
+<template lang="pug">
+.hk-locale-demo
+  .hk-locale-demo-title 语言包
+</template>
+
+<script>
+export default {
+  name: 'hek-locale-demo'
+}
+</script>
+
+<style lang="stylus">
+.hk-locale-demo
+  text-align center
+  &-title
+    font-size 1rem
+    font-weight 600;
+</style>
+
