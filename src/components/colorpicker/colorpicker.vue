41bbb92c2fb4a0cb2f6ad34c176cd7c1a0fdb802
nashaofu <diaocheng@outlook.com>
2018-06-13 18:39:50 +0800

    colorpicker重构

--- src/components/colorpicker/colorpicker.vue
+++ src/components/colorpicker/colorpicker.vue
@@ -8,17 +8,13 @@
       @touchend.prevent="dragend",
       @mousedown="dragstart"
     )
-      canvas(
-        ref="ctx",
-        :width="width",
-        :height="height"
-      )
       .hk-colorpicker-container-canvas-point(:style="getPointStyle")
     .hk-colorpicker-container-disabled(v-if="disabled")
-    .hk-colorpicker-container-image(ref="image")
 </template>
 
 <script>
+import { rgbToHsv, hsvToRgb } from './color'
+
 export default {
   name: 'hk-colorpicker',
   props: {
@@ -38,13 +34,9 @@ export default {
   data () {
     return {
       is: false,
-      ctx: null,
-      width: 280,
-      height: 280,
-      pointerTop: 0,
-      pointerLeft: 0,
-      timer: null,
-      delay: 100
+      x: 0,
+      y: 0,
+      timer: null
     }
   },
   computed: {
@@ -54,17 +46,17 @@ export default {
       const b = this.value.b >= 0 && this.value.b <= 255 ? this.value.b : 0
       return { r, g, b }
     },
+    hsv () {
+      return rgbToHsv(this.rgb)
+    },
     getPointStyle () {
       return {
-        top: `${this.pointerTop}px`,
-        left: `${this.pointerLeft}px`
+        top: `${this.y}px`,
+        left: `${this.x}px`
       }
     }
   },
   mounted () {
-    this.ctx = this.$refs.ctx.getContext('2d')
-    this.width = this.$refs.canvas.offsetWidth
-    this.height = this.$refs.canvas.offsetHeight
     document.addEventListener('mousemove', this.dragging)
     document.addEventListener('mouseup', this.dragend)
     this.draw()
@@ -75,56 +67,19 @@ export default {
   },
   watch: {
     rgb () {
-      this.$nextTick(() => this.setPoint())
+      this.draw()
     }
   },
   methods: {
     draw () {
-      const url = window.getComputedStyle(this.$refs.image).getPropertyValue('background-image')
-      const reg = /https?:\/\/(([a-zA-Z0-9_-])+(\.)?)*(:\d+)?(\/((\.)?(\?)?=?&?[a-zA-Z0-9_-](\?)?)*)*/i
-      this.pointerLeft = this.width / 2
-      this.pointerTop = this.height / 2
-      if (reg.exec(url)[0]) {
-        const $img = new Image()
-        $img.addEventListener('load', () => {
-          this.ctx.drawImage($img, 0, 0, $img.width, $img.height, 0, 0, this.width, this.height)
-          // 异步执行
-          setTimeout(() => this.setPoint(), 100)
-        })
-        $img.src = reg.exec(url)[0]
-      }
-    },
-    setPoint () {
-      if (!this.autopick) return
       const { width, height } = this.$refs.canvas.getBoundingClientRect()
-      const radius = (width + height) / 4
-
-      // 位置偏差
-      const offset = 4
-      // 一列一列的遍历匹配颜色
-      for (let x = 0; x < this.width; x++) {
-        for (let y = 0; y < this.height; y++) {
-          const point = {
-            x: x - width / 2,
-            y: -(y - height / 2)
-          }
-          if (Math.sqrt(Math.pow(point.x, 2) + Math.pow(point.y, 2)) <= radius) {
-            const { data } = this.ctx.getImageData(x, y, 1, 1)
-            const [r, g, b] = data
-            if (r + offset > this.rgb.r &&
-              r - offset < this.rgb.r &&
-              g + offset > this.rgb.g &&
-              g - offset < this.rgb.g &&
-              b + offset > this.rgb.b &&
-              b - offset < this.rgb.b
-              ) {
-              this.pointerLeft = x
-              this.pointerTop = y
-              return
-            }
-          }
-        }
-      }
+      const { h, s } = this.hsv
+      // 角度
+      const rad = h * Math.PI * 2
+      // 圆形半径
+      const r = (width + height) / 4
+      this.x = r + Math.cos(rad) * r * s
+      this.y = r - Math.sin(rad) * r * s
     },
     dragstart (e) {
       if (this.disabled) {
@@ -151,34 +106,44 @@ export default {
       if (this.disabled) {
         return
       }
-      const x = e.changedTouches && e.changedTouches[0] ? e.changedTouches[0].clientX : e.clientX
-      const y = e.changedTouches && e.changedTouches[0] ? e.changedTouches[0].clientY : e.clientY
+      let x = e.changedTouches && e.changedTouches[0] ? e.changedTouches[0].clientX : e.clientX
+      let y = e.changedTouches && e.changedTouches[0] ? e.changedTouches[0].clientY : e.clientY
       const { left, top, width, height } = this.$refs.canvas.getBoundingClientRect()
 
       // 以圆盘中心为坐标原点
-      const radius = (width + height) / 4
-      const point = {
-        x: x - left - width / 2,
-        y: -(y - top - height / 2)
-      }
-      if (Math.sqrt(Math.pow(point.x, 2) + Math.pow(point.y, 2)) > radius) {
-        let rad = Math.atan(point.y / point.x) // 与x轴夹角弧度值
-        if (point.x < 0) { // 第二、三象限
+      const r = (width + height) / 4
+
+      x = x - left - width / 2
+      y = -(y - top - height / 2)
+
+      // 点在圆外时
+      if (Math.sqrt(Math.pow(x, 2) + Math.pow(y, 2)) > r) {
+        let rad = Math.atan(y / x) // 与x轴夹角弧度值
+        if (x < 0) { // 第二、三象限
           rad += Math.PI
-        } else if (point.y < 0 && point.x > 0) { // 第四象限
+        } else if (y < 0 && x > 0) { // 第四象限
           rad += Math.PI * 2
         }
-        point.x = Math.cos(rad) * radius
-        point.y = Math.sin(rad) * radius
+        x = Math.cos(rad) * r
+        y = Math.sin(rad) * r
       }
-      this.pointerTop = -(point.y - radius)
-      this.pointerLeft = point.x + radius
-      // 节流一下
+
+      let { h, s, v } = this.hsv
+      const r1 = Math.sqrt(Math.pow(x, 2) + Math.pow(y, 2))
+      s = r1 / r
+      let rad = Math.acos(x / r1)
+
+      // 角度大于180时处理一下
+      if (y < 0) rad = 2 * Math.PI - rad
+      h = rad / (Math.PI * 2)
+
+      this.x = r + x
+      this.y = r - y
+
       clearTimeout(this.timer)
       this.timer = setTimeout(() => {
-        const [r, g, b] = this.ctx.getImageData(this.pointerLeft, this.pointerTop, 1, 1).data
-        this.$emit('input', { r, g, b })
-      }, this.delay)
+        this.$emit('input', hsvToRgb({ h, s, v }))
+      }, 100)
     }
   }
 }
@@ -199,10 +164,16 @@ $padding = 0.45rem
     overflow hidden
     position relative
     &-canvas
-      width $size
-      height $size
+      background-image url("./color.png")
+      background-size cover
+      background-position center
+      background-repeat no-repeat
       border-radius 50%
-      position relative
+      position absolute
+      top $padding
+      right $padding
+      bottom $padding
+      left $padding
       &-point
         width 0.7rem
         height 0.7rem
@@ -212,9 +183,7 @@ $padding = 0.45rem
         background-color rgba(255,255,255,0.7)
         box-shadow 0 0 0 1.5px #fff, inset 0 0 1px 1px rgba(0,0,0,.3), 0 0 1px 2px rgba(0,0,0,.4)
         cursor pointer
-    &-image
-      background-image url("./color.png")
-      visibility hidden
+
     &-disabled
       position absolute
       top 0
